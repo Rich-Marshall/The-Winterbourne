cd77f1877d3b7df04c1667c5eed9bc3bbcdb2b1a
Your Namerich-marshall <p16189101@my365.dmu.ac.uk>
2019-09-13 18:36:17 +0100

    navbar implemented. Still a few tweaks needed

--- js/app.js
+++ js/app.js
@@ -1,10 +1,28 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
+  const navIn = document.querySelector('.nav-links');
+  const navOut = document.querySelector('.nav-active');
+  const navLinks = document.querySelectorAll('.nav-links li');
 
   burger.addEventListener('click',()=>{
-    nav.classList.toggle('nav-active');
+      //toggle the menu
+      navIn.classList.toggle('nav-active');
+
+      //animate the list of links
+      navLinks.forEach((link, index)=>{
+        if (link.style.animation) {
+          link.style.animation = ''
+        } else {
+          link.style.animation = `navLinkFadeIn 1s ease forwards ${index / 7 + 0.1}s`
+
+        }
+      });
+      //burger cross animation
+      burger.classList.toggle('toggle');
+
   });
+
+
 }
 
 navSlide();
