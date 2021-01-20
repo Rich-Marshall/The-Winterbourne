50ed376c8c39f94e19920e727e0a5030a9fc62d0
Your Namerich-marshall <p16189101@my365.dmu.ac.uk>
2021-01-20 13:32:08 +0000

    added self hiding navbar even on resize

---
+++ js/nav.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+  if($(window).width() >= 1000)
+  {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("nav").style.top = "0";
+    } else {
+      document.getElementById("nav").style.top = "-149px";
+    }
+      prevScrollpos = currentScrollPos;
+    }
+  }
+  else {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("nav").style.top = "0";
+    } else {        document.getElementById("nav").style.top = "-199px";
+    }
+      prevScrollpos = currentScrollPos;
+    }
+  }
+});
+
+$(window).resize(function(){
+  if($(window).width() >= 1000)
+  {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("nav").style.top = "0";
+    } else {
+      document.getElementById("nav").style.top = "-149px";
+    }
+      prevScrollpos = currentScrollPos;
+    }
+  }
+  else {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("nav").style.top = "0";
+    } else {
+      document.getElementById("nav").style.top = "-199px";
+    }
+      prevScrollpos = currentScrollPos;
+    }
+  }
+});
